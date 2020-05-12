14a81ef0b9c3800119356354d105042ebc8163e4
Jonghyun Ho <jonghyun.ho@gmail.com>
2020-05-12 23:46:28 +0900

    Updates

---
+++ precache-manifest.f1c0e9e4fec2d9fb822915bd6aedd848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7030ec14a2046e6653fe852b1a416c",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "fba7a02282be32361997",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/test/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba7a02282be32361997",
+    "url": "/test/static/js/main.0737d285.chunk.js"
+  },
+  {
+    "revision": "f63ecef71b330c334131",
+    "url": "/test/static/js/runtime-main.f77b3668.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
