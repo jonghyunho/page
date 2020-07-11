1cdb0a9370095bc949a77a0968e2448d67fa7026
Jonghyun Ho <jonghyun.ho@gmail.com>
2020-07-12 03:00:49 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/page/precache-manifest.7a8e0e3b4231d5da791a0a7562a4423f.js"
+  "/page/precache-manifest.2ab290e1eaa80cb81ece945ef655063a.js"
 );
 
 self.addEventListener('message', (event) => {
